c839038ed9d94d655bb606be9be34519f0f00fc7
Israel <olaoyeaisrael@gmail.com>
2026-05-19 15:34:34 +0100

    May 7

--- test_llm.py
+++
@@ -1,9 +0,0 @@
-import sys
-from services.model import llm
-from langchain_core.messages import HumanMessage
-
-msg = HumanMessage(content="Reply with OK")
-try:
-    print(llm.invoke([msg]))
-except Exception as e:
-    print("Error:", e)
